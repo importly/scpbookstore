eded7e3b436fa203300772049c08058eecc51b54
importly <aryankuthakur@hotmail.com>
2022-07-04 16:57:15 +0530

    added changes in how id and api for books work

--- src/routes/books/[id].js
+++ src/routes/books/[id].js
@@ -1,18 +1,21 @@
-import { prisma } from "./_prismac";
+import { prisma } from './_prismac';
 
 /** @type {import('./__types/[id]').RequestHandler} */
 export async function get({ params }) {
-    // `params.id` comes from [id].js
-    let item;
-    prisma.book.findFirst();
-   
-    if (item) {
-      return {
-        body: { item }
-      };
-    }
-   
-    return {
-      status: 404
-    };
-  }
+	// `params.id` comes from [id].js
+	let item = await prisma.book.findUnique({ // vv easy db manager
+    where: { 
+      id: Number(params.id)  // look for a unique book with in the book database with the id given by the website.
+    } // id is always unique becuase there is only one id per book, it auto increments.
+  });
+
+	if (item) {
+		return {
+			body: { item } // if we found the book, give the information back.
+		};
+	}
+
+	return {
+		status: 404 // idk what happened, probably didn't find the book
+	};
+}
