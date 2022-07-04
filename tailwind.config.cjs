8b79cfe9c69af5d8e5cdfff09f790f23c22688be
importly <aryankuthakur@hotmail.com>
2022-07-04 13:30:06 +0530

    removed notes section of database

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,35 @@
+const themes = require('daisyui/src/colors/themes');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui')],
+
+	daisyui: {
+		styled: true,
+		themes: [
+			{
+				mytheme: {
+					primary: '#2176AE',
+					secondary: '#2E2E2E',
+					accent: '#FBB037',
+					neutral: '#181A2A',
+					'base-100': '#FFFFFF',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272'
+				}
+			}
+		],
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
+		darkTheme: 'dark'
+	}
 };
