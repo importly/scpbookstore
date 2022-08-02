86f9b49ec72e06872626f01c2c3231b83be025ad
importly <aryankuthakur@gmail.com>
2022-08-02 15:45:55 -0400

    fixed vercel problems

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-node';
 import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +14,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: vercel()
+		adapter: adapter(),
 	}
 };
 
