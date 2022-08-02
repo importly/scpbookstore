6b3b4f23c42c5d8ab31419c2994604adc27aab10
importly <aryankuthakur@gmail.com>
2022-08-02 15:16:32 -0400

    added vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +13,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
