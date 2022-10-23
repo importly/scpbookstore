8dab05b44620a3b90f4a9658c9111b653cfef050
sidhys <sidhys1@gmail.com>
2022-10-23 15:05:58 -0400

    oops

--- src/routes/books/push_book.js
+++ src/routes/books/push_book.js
@@ -3,7 +3,7 @@ import { prisma } from './_prismac';
 /** @type {import('../../../.svelte-kit/types/src/routes/books/__types/push_book').RequestHandler} */
 export async function POST({ params }) {
 
-	int STATUS_CREATED = 201, STATUS_INTERNAL_ERROR = 500, STATUS_NOT_IMPLEMENTED = 501;
+	let STATUS_CREATED = 201, STATUS_INTERNAL_ERROR = 500, STATUS_NOT_IMPLEMENTED = 501;
 
 	let books;  
 
