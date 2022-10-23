ee0f6381db5bf2992ea9e6be5e4268dfdeb0559f
Sid <sidhys1@gmail.com>
2022-10-23 15:04:49 -0400

    volunteer db input

---
+++ src/routes/books/push_book.js
@@ -0,0 +1,23 @@
+import { prisma } from './_prismac';
+
+/** @type {import('../../../.svelte-kit/types/src/routes/books/__types/push_book').RequestHandler} */
+export async function POST({ params }) {
+
+	int STATUS_CREATED = 201, STATUS_INTERNAL_ERROR = 500, STATUS_NOT_IMPLEMENTED = 501;
+
+	let books;  
+
+	books = await prisma.book.create({
+
+        data: {
+          // todo: use params & push data
+          
+        },
+	});
+
+
+	return {
+		status: STATUS_NOT_IMPLEMENTED 
+	};
+
+}
