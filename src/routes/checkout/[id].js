a05532121f12079293d8d68b734517a2e798e757
= <=>
2022-10-28 18:37:25 -0400

    very nice stuff

---
+++ src/routes/checkout/[id].js
@@ -0,0 +1,29 @@
+import { prisma } from '../books/_prismac';
+
+/** @type {import('../../../.svelte-kit/types/src/routes/checkout/__types/[id]').RequestHandler} */
+export async function GET({ params }) {
+	// `params.id` comes from [id].js
+	let item;
+
+	if (isNaN(parseInt(params.id))) return {status: 404};
+	
+	item = await prisma.book.findUnique({
+			// vv easy db manager
+			where: {
+				id: parseInt(params.id) // look for a unique book with in the book database with the id given by the website.
+			} // id is always unique becuase there is only one id per book, it auto increments.
+		});
+
+	if (item) {
+		return {
+			status: 200,
+			headers: {},
+			body: { item } // if we found the book, give the information back.
+		};
+	}
+
+	return {
+		status: 404 // idk what happened, probably didn't find the book
+	};
+}
+// try to add 
---
+++ src/routes/checkout/[id].svelte
@@ -0,0 +1,38 @@
+<script>
+	// populated with data from the endpoint
+	export /**
+	 * @type {{ title: any; image:any; subject:any; description:any; condition:any; year:any; pros:any; cons:any; onlinelink:any;}}
+	 */
+	let item;
+</script>
+
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Checkout now?</h1>
+        <p class="py-6 font-semibold text-lg">{item.title}</p>
+      </div>
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="email" class="input input-bordered" />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="password" class="input input-bordered" />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
