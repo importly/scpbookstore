18168423691403657c01fb767643f5dd0ba6d138
Aryan <52215513+importly@users.noreply.github.com>
2022-11-29 21:48:31 -0500

    books

--- src/routes/books/+page.server.js
+++ src/routes/books/+page.server.js
@@ -1,20 +1,20 @@
 import { error, json } from '@sveltejs/kit';
 import { prisma } from './_prismac';
 
-/** @type {import('../../../.svelte-kit/types/src/routes/book/__types/index').PageServerLoad} */
+/** @type {import('../../../.svelte-kit/types/src/routes/books/__types/index').PageServerLoad} */
 export async function load({ params }) {
 	// `params.id` comes from [id].js
-	let book;
+	let books;
 
-	book = await prisma.book.findMany({
+	books = await prisma.book.findMany({
 		// vv easy db manager
 		where: {
 			ispublic: true // look for a unique book with in the book database with the id given by the website.
 		} // id is always unique becuase there is only one id per book, it auto increments.
 	});
 
-	if (book) {
-		return { book };
+	if (books) {
+		return { books };
 	}
 
 	return error(400, 'not found');
