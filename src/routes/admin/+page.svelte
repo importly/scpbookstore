d78298b62f59234864251718afffb82df6d9557e
Aryan <52215513+importly@users.noreply.github.com>
2023-01-18 18:36:22 -0500

    50 books added

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<!-- make a centered text box that just says Admin Portal-->
+<div class="text-center">
+    <h1>Admin Portal</h1>
+    <input type="text" placeholder="Password" class="input w-full max-w-xs" />
+</div>
---
+++ src/routes/admin/[id]/+page.server.js
@@ -0,0 +1,21 @@
+import { error, json } from '@sveltejs/kit';
+import { prisma } from '../../books/_prismac';
+
+/** @type {import('../../../.svelte-kit/types/src/routes/checkout/__types/[id]').PageServerLoad} */
+export async function load({ params }) {
+	// `params.id` comes from [id].js
+	let book_info = await prisma.book.findFirst({
+		// vv easy db manager
+		where: {
+			hashedId: params.id
+		}
+	});
+	if (!book_info) {
+		return error(404, 'Book not found');
+	}
+	if (book_info.loaner) {
+		return {book_info, status:"You can not check this book out, already checked out."}
+	}
+	return { book_info, status: 'success' };
+}
+// try to add
